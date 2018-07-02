000562a3bb25ac944a216bcd73c3cb0beaef9ea3
Elabar <leehoemun@gmail.com>
2018-07-02 16:41:41 +0800

    Added button condition

--- ElementCalculator/Form1.cs
+++ ElementCalculator/Form1.cs
@@ -25,13 +25,18 @@ namespace ElementCalculator
             int vertical = 40;
 
             Button[] singleArray = new Button[10];
-
+            
+            int[] singleMP = { 60,92,110,140,170,200,230,260,290,320};
             for (int i = 0; i < 10; i++)
             {
                 singleArray[i] = new Button();
                 singleArray[i].Size = new Size(60, 23);
                 singleArray[i].Location = new Point(horizotal, vertical);
                 singleArray[i].Text = "Level " + (i + 1);
+                if ((Convert.ToInt32(MPTextBox.Text) - singleMP[i]) % 3 == 2)
+                    singleArray[i].BackColor = Color.Green;
+                else
+                    singleArray[i].BackColor = Color.Red;
                 horizotal += 70;
                 this.Controls.Add(singleArray[i]);
             }
@@ -41,12 +46,17 @@ namespace ElementCalculator
 
             Button[] threeTargetArray = new Button[10];
 
+            int[] ThreeMP = { 60, 92, 110, 140, 170, 200, 230, 260, 290, 320 };
             for (int i = 0; i < 10; i++)
             {
                 threeTargetArray[i] = new Button();
                 threeTargetArray[i].Size = new Size(60, 23);
                 threeTargetArray[i].Location = new Point(horizotal, vertical);
                 threeTargetArray[i].Text = "Level " + (i + 1);
+                if ((Convert.ToInt32(MPTextBox.Text) - ThreeMP[i]) % 3 == 1)
+                    threeTargetArray[i].BackColor = Color.Green;
+                else
+                    threeTargetArray[i].BackColor = Color.Red;
                 horizotal += 70;
                 this.Controls.Add(threeTargetArray[i]);
             }
@@ -56,12 +66,17 @@ namespace ElementCalculator
 
             Button[] AllTargetArray = new Button[10];
 
+            int[] AllMP = { 60, 92, 160, 140, 170, 200, 230, 260, 290, 320 };
             for (int i = 0; i < 10; i++)
             {
                 AllTargetArray[i] = new Button();
                 AllTargetArray[i].Size = new Size(60, 23);
                 AllTargetArray[i].Location = new Point(horizotal, vertical);
                 AllTargetArray[i].Text = "Level " + (i + 1);
+                if ((Convert.ToInt32(MPTextBox.Text) - AllMP[i]) % 3 == 1)
+                    AllTargetArray[i].BackColor = Color.Green;
+                else
+                    AllTargetArray[i].BackColor = Color.Red;
                 horizotal += 70;
                 this.Controls.Add(AllTargetArray[i]);
             }
